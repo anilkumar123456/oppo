f61b16b0dafffdb991d5e7169fa8c6e4e92519bb
Ubuntu <ubuntu@ip-172-31-46-7.us-east-2.compute.internal>
2018-01-29 10:04:06 +0000

    first commit

---
+++ src/main/java/com/oppo/app/App.java
@@ -0,0 +1,13 @@
+package com.oppo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
